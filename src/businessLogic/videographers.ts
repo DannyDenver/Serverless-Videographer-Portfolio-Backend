cb97452309ec1b6653d7ae5355cd843838845fb4
DannyDenver <djt09d@fulbrightmail.org>
2020-03-01 17:47:51 -0700

    filter out own profile from videographer list

--- src/businessLogic/videographers.ts
+++ src/businessLogic/videographers.ts
@@ -12,22 +12,31 @@ const logger = createLogger('videographer')
 
 export async function getVideographers(event: APIGatewayProxyEvent): Promise<Videographer[]> {
     const token = getJWT(event)
+    let videographerId; 
 
     if (token) {
-        const videographerId = parseUserId(token)
+        let auth0Id = parseUserId(token);
+        videographerId = auth0Id.includes("|") ? auth0Id.split("|")[1] : auth0Id;
         const videographerExists = await videographerAccess.videographerExists(videographerId)
         if (!videographerExists) {
+            if (videographerId.includes("|")){
+                videographerId = videographerId.split("|")[1];
+            }
+
             const newVideographer: Videographer = {
                 id: videographerId
-            }
+            };
+
             logger.info(`Creating videographer.`, newVideographer);
 
-            await videographerAccess.createVideographer(newVideographer)
+            await videographerAccess.createVideographer(newVideographer);
         }        
     }
 
-    logger.info('Getting all videographers')
-    return await videographerAccess.getVideographers()
+    logger.info('Getting all videographers');
+    const videographers = await videographerAccess.getVideographers();
+
+    return videographers.filter(videographer => videographer.id !==  videographerId && videographer.firstName && videographer.lastName);
 }
 
 export async function getVideographer(event: APIGatewayProxyEvent): Promise<Videographer> {
