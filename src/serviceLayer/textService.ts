5a806694f3ed544e754a7d12bd35db2993734a73
DannyDenver <djt09d@fulbrightmail.org>
2020-03-20 19:23:51 -0600

    add sms service with twilio

---
+++ src/serviceLayer/textService.ts
@@ -0,0 +1,26 @@
+import * as AWS from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
+
+const XAWS = AWSXRay.captureAWS(AWS)
+const manager = new XAWS.SSM()
+
+export class TextMessageService {
+    constructor() { }
+
+    async sendMessage(contact: string, message: string) {
+        console.log('contact and messages', contact, message);
+
+        const accountSid = await manager.getParameter({ Name: 'TwilioAccountId' }).promise();
+        const authToken = await manager.getParameter({ Name: 'TwilioAuthToken', WithDecryption: true }).promise();
+        const twilioNumber = await manager.getParameter({ Name: 'TwilioNumber' }).promise();
+        const client = require('twilio')(accountSid.Parameter.Value, authToken.Parameter.Value);
+
+        // client.messages
+        //     .create({
+        //         body: message,
+        //         from: twilioNumber.Parameter.Value,
+        //         to: contact
+        //     })
+        //     .then(message => console.log(message.sid));
+    }
+}
